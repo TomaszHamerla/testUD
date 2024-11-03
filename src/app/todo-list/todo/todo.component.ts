7965afce439f36f77e38916c3ff5bcac99790335
unknown <tomaszhamerla@interia.pl>
2024-11-03 13:10:35 +0100

    hooks test

--- src/app/todo-list/todo/todo.component.ts
+++ src/app/todo-list/todo/todo.component.ts
@@ -1,4 +1,14 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+  Component,
+  DoCheck,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges
+} from '@angular/core';
 import {Todo} from "../../shared/interfaces/Todo";
 
 @Component({
@@ -6,12 +16,38 @@ import {Todo} from "../../shared/interfaces/Todo";
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
-export class TodoComponent {
+//implements OnChanges
+//
+//implements DoCheck
+export class TodoComponent implements OnInit, OnDestroy {
   @Input() todo!: Todo;
   @Input() i!: number;
   @Output() delete = new EventEmitter<void>;
-
   openModal = false;
+  timeout: any;
+
+  // constructor() {
+  //   console.log(this.todo) -> undefined
+  // }
+
+  ngOnInit(): void {
+   this.timeout = setTimeout(() => {
+      console.log('onInit')
+    },3000)
+    //console.log(this.todo) -> value correct
+  }
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   console.log(changes)
+  // }
+
+  // ngDoCheck(): void {
+  // }
+
+  ngOnDestroy(): void {
+    // console.log("stopTimeout")
+    // clearTimeout(this.timeout);
+  }
 
   changeTodoStatus(todo: Todo) {
     todo.isComplete = !todo.isComplete;
